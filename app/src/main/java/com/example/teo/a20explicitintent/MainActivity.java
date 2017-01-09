b4cf415d3757597d4ef4776544a80c2c38d00f6a
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-09 23:30:15 +0700

    Cố một số lỗi chưa sữa được nên tìm lỗi

--- app/src/main/java/com/example/teo/a20explicitintent/MainActivity.java
+++ app/src/main/java/com/example/teo/a20explicitintent/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.teo.a20explicitintent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    /** Called when the activity is the created.*/
+    //Được gọi đầu tiên khi gọi tạo mới
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+                i.putExtra("Value1","Android By Javatpont");
+                i.putExtra("Value2","Simple Tutorial");
+                //Set the request code to any code you like, you can identify class call via this code
+                //Đặt mã yêu cầu cho bất kí mã nào mà bạn thích, bạn có thể nhận gọi thông qua <code></code>
+                startActivity(i);
+            }
+        });
     }
 }

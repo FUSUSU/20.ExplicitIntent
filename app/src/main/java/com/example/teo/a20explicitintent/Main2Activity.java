e29f7cd8d5925b88f81416ad3eb2c0d0d84b549f
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-10 11:13:15 +0700

    Bắt sự kiện click button rồi chuyển màn hình sang activitymain

--- app/src/main/java/com/example/teo/a20explicitintent/Main2Activity.java
+++ app/src/main/java/com/example/teo/a20explicitintent/Main2Activity.java
@@ -1,8 +1,10 @@
 package com.example.teo.a20explicitintent;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,5 +24,16 @@ public class Main2Activity extends Activity {
         String value1 = extras.getString("Value1");
         String value2 = extras.getString("Value2");
         Toast.makeText(getApplicationContext(), "Value are:\n First value: "+value1+"\n Second Value: "+value2, Toast.LENGTH_LONG).show();
+
+        //Bắt sự kiện click button rồi chuyển màn hình
+        Button button1 = (Button) findViewById(R.id.button01);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
